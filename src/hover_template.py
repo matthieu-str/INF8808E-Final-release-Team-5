ba4e8312dac2896e4c4b2714fc76d36e70f9c947
Mohamed Allam <64670984+MOHAMED45457555@users.noreply.github.com>
2023-06-19 15:42:41 -0400

    hover_template.py

---
+++ src/hover_template.py
@@ -0,0 +1,10 @@
+## hover templet for Radar chart
+def get_hover_univ(x_freq, y_freq, universities):
+    hovertext_master = [f'<b>Maîtrise</b><br>University: {uni}<br>No. of Theses: {x_freq[uni] if uni in x_freq else 0}' for uni in universities]
+    hovertext_doctorat = [f'<b>Doctorat</b><br>University: {uni}<br>No. of Theses: {y_freq[uni] if uni in y_freq else 0}' for uni in universities]
+    return hovertext_master, hovertext_doctorat
+
+def get_hover_discipline():
+    hovertemplate_master = '<b>Maîtrise</b><br>Discipline: %{theta}<br>No. of Theses: %{r}<extra></extra>'
+    hovertemplate_doctorat = '<b>Doctorat</b><br>Discipline: %{theta}<br>No. of Theses: %{r}<extra></extra>'
+    return hovertemplate_master, hovertemplate_doctorat

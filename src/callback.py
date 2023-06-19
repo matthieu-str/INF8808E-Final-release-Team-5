9e2ed3269d5db79a87b4f113842c2f4844f8bc76
Shimbi Masengo Wa Umba Papa Levi <70257592+Lukushi@users.noreply.github.com>
2023-06-19 02:55:12 -0400

    Update callback.py

--- src/callback.py
+++ src/callback.py
@@ -1,1 +1,19 @@
-
+def update_navlink_styles(pathname):
+    "Helper function to render the navigation links"
+    
+    styles = [{"backgroundColor": "grey", "border-radius": "30px"} for _ in range(7)]
+    if pathname == "/":
+        styles[0]["backgroundColor"] = "blue"
+    elif pathname == "/stacked-area":
+        styles[1]["backgroundColor"] = "blue"
+    elif pathname == "/stacked-bar":
+        styles[2]["backgroundColor"] = "blue"
+    elif pathname == "/radar":
+        styles[3]["backgroundColor"] = "blue"
+    elif pathname == "/sunburst":
+        styles[4]["backgroundColor"] = "blue"
+    elif pathname == "/back-to-back-bar":
+        styles[5]["backgroundColor"] = "blue"
+    elif pathname == "/box-plots":
+        styles[6]["backgroundColor"] = "blue"
+    return styles

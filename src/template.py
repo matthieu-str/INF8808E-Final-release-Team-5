07660f5febf8fb2c552e57379b55509b292b7fff
Shimbi Masengo Wa Umba Papa Levi <70257592+Lukushi@users.noreply.github.com>
2023-06-19 02:53:37 -0400

    Update template.py

---
+++ src/template.py
@@ -0,0 +1,8 @@
+external_css = [
+    "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css",
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://fonts.googleapis.com/css?family=Open+Sans:400,700",
+    "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css",
+    "assets/css/style.css",
+]

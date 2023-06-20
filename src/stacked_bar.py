ae27b5c3d5065c6676ea8dfb4a31a15feea650db
bessem248 <79014688+bessem248@users.noreply.github.com>
2023-06-20 15:14:35 -0400

    Update stacked_bar.py

--- src/stacked_bar.py
+++ src/stacked_bar.py
@@ -1,13 +1,17 @@
 import plotly.express as px
-
+from preprocess import rename_languages
 
 def get_figure(data, colored, domaine):
     data = data[data['domaine'] == domaine]
     if colored == 'range of pages':
         data = data[data['range of pages'] != '0']
     if colored == 'univ':
-        top_10_univ=list(df.groupby(['univ']).size().reset_index(name='counts').sort_values(by='counts', ascending=False)['univ'].head(10))
-        data = data[data['univ'].isin(top_10_univ)]
+        top_9_universities=list(data.groupby(['univ']).size().reset_index(name='counts').sort_values(by='counts', ascending=False)['univ'].head(9))
+        data.loc[~data['univ'].isin(top_9_universities), 'univ'] = "Other universities"
+    if colored == 'langue':
+        data=rename_languages(data)
+        top_languages=list(data.groupby(['langue']).size().reset_index(name='counts').sort_values(by='counts', ascending=False)['langue'].head(2))
+        data.loc[~data['langue'].isin(top_languages), 'langue'] = "other languages"
     df_grouped = data.groupby(['discipline', colored]).size().unstack().fillna(0)
 
     # Reset the index to have 'discipline' as a column
@@ -20,7 +24,8 @@ def get_figure(data, colored, domaine):
     df_grouped = df_grouped.sort_values('total', ascending=True)
 
     # Create the stacked bar chart using Plotly Express
-    fig = px.bar(df_grouped, y='discipline', x=list(data[colored].unique()), title='Visualisation of the distribution of different variables for various disciplines',orientation='h',
+    fig = px.bar(df_grouped, y='discipline', x=list(data[colored].unique()), 
+            title='Visualisation of the distribution of different variables for various disciplines', orientation='h',
              barmode='stack')
     # Update the x-axis label using plotly graph object
     fig.update_xaxes(title_text="Number of publications")

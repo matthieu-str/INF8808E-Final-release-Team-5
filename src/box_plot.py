f499209c890abcca8d98329b9774b5b14104b22a
Shimbi Masengo Wa Umba Papa Levi <70257592+Lukushi@users.noreply.github.com>
2023-06-21 05:06:32 +0000

    update box plot

--- src/box_plot.py
+++ src/box_plot.py
@@ -4,18 +4,26 @@ from hover_template import get_hover_box_plot
 
 
 def overview_box_plot(filtered_df):
+
     fig = px.box(filtered_df, x="année", y="pages")
     fig.update_layout(
-        title="Box Plot of Number of Pages by Year",
+        title={
+            'text': "Box Plot of Number of Pages by Year",
+            'x': 0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'
+            },
         xaxis_title="Year",
         yaxis_title="Number of Pages",
         showlegend=True)
     fig.update_traces(
         hovertemplate=get_hover_box_plot(),
         marker=dict(color='black'))
+
     return fig
 
 def mvd_box_plot(filtered_df_maitrise, filtered_df_doctorat):
+
     fig = go.Figure()
     fig.add_trace(
         go.Box(
@@ -33,9 +41,15 @@ def mvd_box_plot(filtered_df_maitrise, filtered_df_doctorat):
         )
 
     fig.update_layout(
-        title="Comparison of Number of Pages by Year for Maîtrise and Doctorat",
+        title={
+            'text': "Comparison of Number of Pages by Year for Maîtrise and Doctorat",
+            'x': 0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'
+            },
         xaxis_title="Year",
         yaxis_title="Number of Pages",
         boxmode="group",
         showlegend=True)
+
     return fig

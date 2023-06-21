51f769f130ebdc8662a0d4b9fa30dcd7d78136bd
Shimbi Masengo Wa Umba Papa Levi <70257592+Lukushi@users.noreply.github.com>
2023-06-20 20:51:14 -0400

    functions to draw the box plots

--- src/box_plot.py
+++ src/box_plot.py
@@ -1,1 +1,32 @@
+import plotly.express as px
+import plotly.graph_objects as go
 
+def overview_box_plot(filtered_df):
+    fig = px.box(filtered_df, x="année", y="pages")
+    fig.update_layout(
+        title="Box Plot of Number of Pages by Year",
+        xaxis_title="Year",
+        yaxis_title="Number of Pages",
+        showlegend=True)
+    return fig
+
+def mvd_box_plot(filtered_df_maitrise, filtered_df_doctorat):
+    fig = go.Figure()
+    fig.add_trace(
+        go.Box(
+            x=filtered_df_maitrise["année"],
+            y=filtered_df_maitrise["pages"],
+            name="Maîtrise"))
+    fig.add_trace(
+        go.Box(
+            x=filtered_df_doctorat["année"],
+            y=filtered_df_doctorat["pages"],
+            name="Doctorat"))
+
+    fig.update_layout(
+        title="Comparison of Number of Pages by Year for Maîtrise and Doctorat",
+        xaxis_title="Year",
+        yaxis_title="Number of Pages",
+        boxmode="group",
+        showlegend=True)
+    return fig

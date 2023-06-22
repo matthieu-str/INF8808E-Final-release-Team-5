5fca3fadb65f38f891ddf5f01d137c42eaea1a5f
Mohamed Ismail Allam <64670984+MOHAMED45457555@users.noreply.github.com>
2023-06-22 09:36:22 -0400

    Update hover_template.py

--- src/hover_template.py
+++ src/hover_template.py
@@ -1,12 +1,12 @@
 ## hover templet for Radar chart
 def get_hover_univ(x_freq, y_freq, universities):
     hovertext_master = [f'<b>Maîtrise</b><br>Université: {uni}<br>No. de thèses: {x_freq[uni] if uni in x_freq else 0}' for uni in universities]
-    hovertext_doctorat = [f'<b>Doctorat</b><br>Université: {uni}<br>No. de thèses: {y_freq[uni] if uni in y_freq else 0}' for uni in universities]
+    hovertext_doctorat = [f'<b>Doctorat</b><br>Université: {uni}<br>No. de dissertations: {y_freq[uni] if uni in y_freq else 0}' for uni in universities]
     return hovertext_master, hovertext_doctorat
 
 def get_hover_discipline():
     hovertemplate_master = '<b>Maîtrise</b><br>Discipline: %{theta}<br>No. de thèses: %{r}<extra></extra>'
-    hovertemplate_doctorat = '<b>Doctorat</b><br>Discipline: %{theta}<br>No. de thèses: %{r}<extra></extra>'
+    hovertemplate_doctorat = '<b>Doctorat</b><br>Discipline: %{theta}<br>No. de dissertations: %{r}<extra></extra>'
     return hovertemplate_master, hovertemplate_doctorat
     
 def get_hover_stacked_bar_chart(): 

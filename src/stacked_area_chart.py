d0166e20fbba2891afbcc70639710c5372fbbb12
matthieu-str <matthieu.souttre@gmail.com>
2023-06-22 12:01:31 -0400

    changed ordering of areas and legend

--- src/stacked_area_chart.py
+++ src/stacked_area_chart.py
@@ -3,8 +3,6 @@ import pandas as pd
 from preprocess import other_languages, other_univ, rename_languages, rename_inclassable
 
 # TODO
-# Change colors
-# Change background
 # Text box top 5 disciplines for domains
 
 def get_figure(df, y_axis, mode):
@@ -35,10 +33,12 @@ def get_figure(df, y_axis, mode):
         df_count = pd.merge(left=df_count, right=count_tot, how="left", on="année")
         df_count["percentage"] = round(100 * df_count["count"] / df_count["count_total"], 2)
 
-    fig = px.area(df_count, x="année", y=f"{mode}",
+    fig = px.area(df_count.sort_values(y_axis, ascending=False), # sorting values for displaying areas in alphabetical order
+                  x="année",
+                  y=f"{mode}",
                   color=f"{y_axis}",
                   labels={f'{y_axis}': f'{titles[y_axis]}', f'{mode}': f'{titles[mode]}', 'année': 'Année'})
-
+    fig.update_layout(legend_traceorder='reversed') # reverse ordering in the legend for having top layer the same as top legend
     fig.update_layout(height=600, width=1200)
     fig.update_xaxes(title_text="Année", title_font=dict(size=20))
     if mode == 'count':

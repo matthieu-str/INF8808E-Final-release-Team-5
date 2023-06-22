bbeedd0233f01b9d7becc76e5b3232fd5495d29a
bessem248 <79014688+bessem248@users.noreply.github.com>
2023-06-21 22:57:53 -0400

    Update stacked_bar.py

--- src/stacked_bar.py
+++ src/stacked_bar.py
@@ -1,14 +1,17 @@
-from preprocess import rename_languages
+from preprocess import rename_languages,rename_inclassable
 from hover_template import get_hover_stacked_bar_chart
 import plotly.express as px
+
+Variable_options = {'grade': "des niveaux d'études", 'univ': 'des universités', 'langue': 'des langues',
+                    'range of pages': 'du nombre de pages'}
 
 def get_figure(data, colored, domaine):
     data = data[data['domaine'] == domaine]
     if colored == 'range of pages':
         data = data[data['range of pages'] != '0']
     if colored == 'univ':
-        top_9_universities=list(data.groupby(['univ']).size().reset_index(name='counts').sort_values(by='counts', ascending=False)['univ'].head(9))
-        data.loc[~data['univ'].isin(top_9_universities), 'univ'] = "Other universities"
+        top_5_universities=list(data.groupby(['univ']).size().reset_index(name='counts').sort_values(by='counts', ascending=False)['univ'].head(5))
+        data.loc[~data['univ'].isin(top_5_universities), 'univ'] = "Other universities"
     if colored == 'langue':
         data=rename_languages(data)
         top_languages=list(data.groupby(['langue']).size().reset_index(name='counts').sort_values(by='counts', ascending=False)['langue'].head(2))
@@ -26,7 +29,7 @@ def get_figure(data, colored, domaine):
 
     # Create the stacked bar chart using Plotly Express
     fig = px.bar(df_grouped, y='discipline', x=list(data[colored].unique()), 
-            title='Visualisation of the distribution of different variables for various disciplines', orientation='h',
+            title="Visualisation de la distribution "+Variable_options[colored]+" dans les disciplines du domaine "+domaine, orientation='h',
              barmode='stack')
     fig.update_traces(hovertemplate=get_hover_stacked_bar_chart())
     # Update the x-axis label using plotly graph object
@@ -34,5 +37,5 @@ def get_figure(data, colored, domaine):
     # Update the y-axis label using plotly graph object
     fig.update_yaxes(title_text="Discipline")
     # Size of figure
-    fig.update_layout(height=900, width=900)
+    fig.update_layout(height=900, width=900, legend_title='Catégories '+Variable_options[colored])
     return fig

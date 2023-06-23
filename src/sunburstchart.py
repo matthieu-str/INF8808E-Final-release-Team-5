198143034b3f9e9d1a43f3ff33ea649732b91954
bessem248 <79014688+bessem248@users.noreply.github.com>
2023-06-23 19:29:42 -0400

    Update sunburstchart.py

--- src/sunburstchart.py
+++ src/sunburstchart.py
@@ -99,19 +99,18 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from hover_template import get_hover_sunburst_chart_langue, get_hover_sunburst_chart_univ
+from preprocess import rename_inclassable
 
 def sunburst(df, mode):
-    df['langue_new'] = df['langue'].str.lower().map({'fr': 'fr', 'en': 'en'}).fillna('les autres')
-    language_map = {"fr": "Français", "en": "Anglais", "les autres": "les autres"}
+    df=rename_inclassable(df)
+    df['langue_new'] = df['langue'].str.lower().map({'fr': 'fr', 'en': 'en'}).fillna('autres langues')
+    language_map = {"fr": "français", "en": "anglais", "les autres": "autres langues"}
     df['langue_new'] = df['langue_new'].map(language_map)
     # df = df[~df['domaine'].isin(['inclassable'])]
     # df = df[~df['langue_new'].isin(['les autres'])]
-
     df_domain_languages = df.groupby(["domaine", "langue_new"], as_index=False).count()[
         ["domaine", "langue_new", "grade"]]
     df_domain_languages.rename(columns={"grade": "count"}, inplace=True)
-
-
     df_domain_uni = df.groupby(["domaine", "univ"], as_index=False).count()[["domaine", "univ", "grade"]]
     df_domain_uni.rename(columns={"grade": "count"}, inplace=True)
     # df_uni_grouped = df_domain_uni.groupby('domaine').apply(lambda x: x.nlargest(10, 'count')).reset_index(drop=True)
@@ -120,7 +119,6 @@ def sunburst(df, mode):
     # df_uni_others_grouped = df_uni_others.groupby('domaine')['count'].sum().reset_index()
     # df_uni_others_grouped['univ'] = 'the others'
     # df_uni_combined = pd.concat([df_uni_grouped, df_uni_others_grouped])
-
 
     if mode == 'langue':
         # fig = px.sunburst(df_domain_languages, path=['domaine', 'langue_new'], values='count', custom_data=['langue_new'])
@@ -133,8 +131,6 @@ def sunburst(df, mode):
             labels.append(domain)
             parents.append('')
             values.append(domain_values)
-            if domain == 'inclassable':
-                continue
             for row in domain_df.itertuples():
                 labels.append(f"{domain}-{row.langue_new}")
                 parents.append(domain)
@@ -161,7 +157,7 @@ def sunburst(df, mode):
             #             'rgba(0, 0, 255, 1)', 'rgba(0, 0, 255, 1)', 'rgba(0, 0, 255, 1)', 'rgba(0, 0, 255, 1)'],
             # ),
         ))
-        fig.update_layout(width=800, height=800,
+        fig.update_layout(width=700, height=700,
                           title='La répartition des langues dans différents domaines',
                           annotations=[dict(
                             text='Cliquez sur chaque partie de domaine pour voir plus de détails',
@@ -172,8 +168,8 @@ def sunburst(df, mode):
                          )
         fig.update_traces(
             hovertemplate=get_hover_sunburst_chart_langue(),
-            leaf=dict(opacity=1)
-        )
+            leaf=dict(opacity=1),
+           )
 
     elif mode == 'univ':
         # fig = px.sunburst(df_domain_uni, path=['domaine', 'univ'], values='count', custom_data=['univ'])
@@ -183,21 +179,29 @@ def sunburst(df, mode):
 
         for domain, domain_df in df_domain_uni.groupby('domaine'):
             domain_values = domain_df['count'].sum()
-            top10_children = domain_df.nlargest(10, "count")
-
+            top5_children = domain_df.nlargest(5, "count")
+            top2_children = domain_df.nlargest(2, "count")
             labels.append(domain)
             parents.append('')
             values.append(domain_values)
-            if domain == 'inclassable':
-                continue
-            for row in top10_children.itertuples():
-                labels.append(f"{domain}-{row.univ}")
+            if domain == 'programme individualisé ou inconnu':
+                for row in top2_children.itertuples():
+                    labels.append(f"{domain}-{row.univ}")
+                    parents.append(domain)
+                    values.append(row.count)
+                other_count = domain_values - top2_children["count"].sum()
+                labels.append(f"{domain}-Autres universités")
                 parents.append(domain)
-                values.append(row.count)
-            other_count = domain_values - top10_children["count"].sum()
-            labels.append(f"{domain}-les autres")
-            parents.append(domain)
-            values.append(other_count)
+                values.append(other_count)
+            else:
+                for row in top5_children.itertuples():
+                    labels.append(f"{domain}-{row.univ}")
+                    parents.append(domain)
+                    values.append(row.count)
+                other_count = domain_values - top5_children["count"].sum()
+                labels.append(f"{domain}-Autres universités")
+                parents.append(domain)
+                values.append(other_count)
 
 
         fig = go.Figure()
@@ -217,8 +221,8 @@ def sunburst(df, mode):
             text=[label.split('-')[-1].strip() for label in labels],
         ))
 
-        fig.update_layout(width=800, height=800,
-                          title='La répartition des 10 meilleures universités dans différents domaines',
+        fig.update_layout(width=700, height=700,
+                          title='La répartition des meilleures universités dans différents domaines',
                           annotations=[dict(
                               text='Cliquez sur chaque partie de domaine pour voir plus de détails',
                               x=-0.05, y=1.05,

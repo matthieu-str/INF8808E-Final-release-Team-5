28b0dffc3a39b51fb2fce1c936477f4c550eb139
bessem248 <79014688+bessem248@users.noreply.github.com>
2023-06-24 12:59:46 -0400

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,22 +1,6 @@
 '''
 Contains the server to run our application.
 '''
-
-from flask_failsafe import failsafe
 from app import app
 
-
-@failsafe
-def create_app():
-    '''
-        Gets the underlying Flask server from the Dash app.
-
-        Returns:
-            The server to be run
-    '''
-
-    return app.server
-
-
-if __name__ == "__main__":
-    create_app().run(port="8060", debug=True)
+server = app.server

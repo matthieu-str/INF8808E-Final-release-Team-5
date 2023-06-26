ac0813dd0ebea41daacc0cc0d3448aa6dac1b3fe
Shimbi Masengo Wa Umba Papa Levi <70257592+Lukushi@users.noreply.github.com>
2023-06-25 20:57:17 -0400

    Update box_plot.py hovertemplate

--- src/box_plot.py
+++ src/box_plot.py
@@ -1,15 +1,27 @@
-import plotly.express as px
+'''
+    Contains two functions related to the creation of the Box plots.
+'''
+
 import plotly.graph_objects as go
 from hover_template import get_hover_box_plot
 import plotly.colors
 
-
 def overview_box_plot(filtered_df, context_title):
+    '''
+        return Box plots of the number of pages per year for all grades.
+    '''
 
     text =  "Diagramme en Boîte du nombre de pages par année"
     title_text = text + context_title
-    fig = px.box(filtered_df, x="année", y="pages")
+    
     color_palette = ["rgb(0, 0, 0)"] + plotly.colors.sequential.Viridis
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Box(
+            x=filtered_df["année"],
+            y=filtered_df["pages"],
+            name="grade"))
 
     fig.update_layout(
         title={
@@ -20,18 +32,23 @@ def overview_box_plot(filtered_df, context_title):
             },
         xaxis_title="Année",
         yaxis_title="Nombre de Pages",
-        showlegend=True)
+        showlegend=False)
     fig.update_traces(
         hovertemplate=get_hover_box_plot(name_text=None),
         marker=dict(color=color_palette[3]))
+    fig.update_layout(hovermode='x unified', )
+    fig.update_traces(hoveron="boxes", hoverinfo='all') 
 
     return fig
 
 def mvd_box_plot(filtered_df_maitrise, filtered_df_doctorat, context_title):
-    
+    '''
+        return Box plots of the number of pages per year for Doctorat vs Maîtrise.
+    '''
+
     text = "Comparaison du nombre de pages par année à la Maîtrise et au Doctorat"
     title_text = text + context_title
-    
+
     fig = go.Figure()
     master = "Maîtrise"
     phd = "Doctorat"
@@ -44,7 +61,7 @@ def mvd_box_plot(filtered_df_maitrise, filtered_df_doctorat, context_title):
             name=master,
             hovertemplate=get_hover_box_plot(name_text=master),
             marker=dict(color=color_palette[0])),)
-    
+
     fig.add_trace(
         go.Box(
             x=filtered_df_doctorat["année"],
@@ -64,5 +81,7 @@ def mvd_box_plot(filtered_df_maitrise, filtered_df_doctorat, context_title):
         yaxis_title="Nombre de Pages",
         boxmode="group",
         showlegend=True)
+    fig.update_layout(hovermode='x unified', )
+    fig.update_traces(hoveron="boxes", hoverinfo='all')
 
     return fig

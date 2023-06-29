a9c4fe4a384b64a545b6dfa4e08521947b6ea257
bessem248 <79014688+bessem248@users.noreply.github.com>
2023-06-29 18:10:10 -0400

    Update helper.py

--- src/helper.py
+++ src/helper.py
@@ -1,23 +1,25 @@
+
 '''
     This file contains some helper functions to help display some figures.
 '''
-import plotly.graph_objects as go
+
 
 def update_box_layout(fig, title_text, showlegend=True):
-  '''
-    return Box plots of the number of pages per year for all grades.
-  '''  
-  fig.update_layout(
-        title={
-            'text': title_text,
-            'x': 0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'
-            },
-        xaxis_title="Année",
-        yaxis_title="Nombre de Pages",
-        hovermode='x unified',
-        showlegend=showlegend)
-  return fig
+    '''
+        return Box plots of the number of pages per year for all grades.
+    '''
+    fig.update_layout(
+            title={
+                'text': title_text,
+                'x': 0.5,
+                'xanchor': 'center',
+                'yanchor': 'top'
+                },
+            xaxis_title="Année",
+            yaxis_title="Nombre de Pages",
+            hovermode='x unified',
+            showlegend=showlegend)
+    return fig
+
 
 

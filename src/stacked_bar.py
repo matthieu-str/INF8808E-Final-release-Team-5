a05002928ddbae28426bc2a3a7895e3d7509cc9c
bessem248 <79014688+bessem248@users.noreply.github.com>
2023-06-29 17:39:37 -0400

    Update stacked_bar.py

--- src/stacked_bar.py
+++ src/stacked_bar.py
@@ -1,96 +1,83 @@
+'''
+Import libraries
+'''
 import plotly.express as px
-import pandas as pd
-from preprocess import other_languages, other_univ, rename_languages, rename_inclassable, get_top_univ
-from hover_template import get_hover_stacked_area_chart
+from preprocess import rename_languages
+from hover_template import get_hover_stacked_bar_chart
+
+# This variable is used to make a proper display of the title of figures
+Variable_options = {'grade': "des niveaux d'études", 'univ': 'des universités',
+                    'langue': 'des langues',
+                    'range of pages': 'du nombre de pages'}
 
 
-def get_figure(df, y_axis, mode):
+def get_figure(data, colored, domaine):
     '''
-    This function generates the stacked area figure, with its different y-axis and mode possibilities.
-    :param df: (pandas dataframe) data to visualize after basic pre-processing
-    :param y_axis: (str) y-axis to visualize, can be 'univ', 'domaine', 'langue' or 'grade'
-    :param mode: (str) mode of the chart, can be 'count' for counting the number of publications or 'percentage' for normalized values.
-    :return: (figure) the stacked area chart
+    This function generates a stacked bar chart showing the distribution
+    of the number of publications for different disciplines and for different
+    variables such as universities, languages and degree levels
+    Inputs :
+    df : is the data frame that contains all the information.
+    colored : takes the variable that will be studied (universities, languages,
+    degree levels or number of pages) based on the number of publications.
+    domaine : contains the major field.
+
+    Output : It returns the stacked bar chart.
+
     '''
-    titles = {'univ': 'Université',
-              'domaine': 'Domaine',
-              'langue': 'Langue',
-              'grade': "Niveau d'études"}
+    data = data[data['domaine'] == domaine]
+    if colored == 'range of pages':
+        # Take only the significant number of pages
+        data = data[data['range of pages'] != '0']
+    if colored == 'univ':
+        # Take only top 5 universities since it is hard
+        # to see them all in same graphic
+        top_5_universities = list(data.groupby(['univ']).size().reset_index(name='counts').sort_values(by='counts', ascending=False)['univ'].head(5))
+        data.loc[~data['univ'].isin(top_5_universities), 'univ'] = "Autres universités"
+    if colored == 'langue':
+        # Take only top used languages since
+        # it is hard to see them all in same graphic
+        data = rename_languages(data)
+        top_languages = list(data.groupby(['langue']).size().reset_index(name='counts').sort_values(by='counts', ascending=False)['langue'].head(2))
+        data.loc[~data['langue'].isin(top_languages), 'langue'] = "autres langues"
+    df_grouped = data.groupby(['discipline', colored]).size().unstack().fillna(0)
 
-    df_count = df.groupby(by=['année', f'{y_axis}'], as_index=False).count()[['année', f'{y_axis}', 'discipline']]
-    df_count.rename(columns={"discipline": "count"}, inplace=True)
+    # Reset the index to have 'discipline' as a column
+    df_grouped = df_grouped.reset_index()
 
-    if y_axis == 'univ':
-        top_univ = get_top_univ(df_count, n=5)
-        df_count = other_univ(df_count, True, top_univ)
-        df = other_univ(df, False, top_univ)
+    # Calculate the total number of degrees for each discipline
+    df_grouped['total'] = sum([df_grouped[col] for col in list(data[colored].unique())])
 
-    if y_axis == 'domaine':
-        df_count = rename_inclassable(df_count)
-        df = rename_inclassable(df)
+    # Sort the DataFrame based on the total number of degrees
+    # in descending order
+    df_grouped = df_grouped.sort_values('total', ascending=True)
 
-    if y_axis == 'langue':
-        df_count = other_languages(df_count, True)
-        df_count = rename_languages(df_count)
-        df = other_languages(df, False)
-        df = rename_languages(df)
-
-    if mode == "percentage":
-        count_tot = df_count.groupby(["année"], as_index=False).sum("count")
-        count_tot.rename(columns={"count": "count_total"}, inplace=True)
-        df_count = pd.merge(left=df_count, right=count_tot, how="left", on="année")
-        df_count["percentage"] = round(100 * df_count["count"] / df_count["count_total"], 2)
-
-    top_disciplines = df.groupby(['année', f'{y_axis}'])['discipline'].apply(
-        lambda x: '<br>' + '<br>'.join([
-            f'{i} ({round(j, 2)}{"%" if mode=="percentage" else ""})' for i, j in zip(
-                x.value_counts().nlargest(5).index,
-                (x.value_counts().nlargest(5)/x.value_counts().sum()*100 if mode=="percentage" else x.value_counts().nlargest(5).values)
-            )
-        ])
-    ).reset_index()
-
-    df_count = pd.merge(df_count, top_disciplines, on=['année', f'{y_axis}'])
-    df_count.rename(columns={'discipline': 'd'}, inplace=True)
-    hover_data = {f'{y_axis}': False, "d": True}
-    df_count = df_count.sort_values(y_axis, ascending=False)
-    fig = px.area(df_count,
-                  x="année",
-                  y=f"{mode}",
-                  color=f"{y_axis}",
-                  labels={f'{y_axis}': f'{titles[y_axis]}'},
-                  custom_data=['d'],
-                  hover_data=hover_data
-                  )
-    fig.update_traces(hovertemplate=get_hover_stacked_area_chart(mode))
-    fig.update_layout(legend_traceorder='reversed')
-    fig.update_layout(height=600, width=1200)
-    fig.update_xaxes(title_text="Année", title_font=dict(size=15))
-    fig.add_annotation(text=f'Evolution du nombre de publications par {titles[y_axis].lower()} en fonction du temps',
-                       # x = 0.95,
-                       y = 1.2,
+    # Create the stacked bar chart using Plotly Express
+    fig = px.bar(df_grouped, y='discipline', x=list(data[colored].unique()),
+                 orientation='h', barmode='stack')
+    fig.update_traces(hovertemplate=get_hover_stacked_bar_chart())
+    # Update the x-axis label using plotly graph object
+    fig.update_xaxes(title_text="Nombre de publications",
+                     title_font=dict(size=15))
+    # Update the layout to remove the y-axis label
+    fig.update_layout(yaxis_title="")
+    # Update the y-axis label using plotly graph object
+    fig.update_layout(title=dict(text='Discipline',
+                                 xanchor='left',
+                                 yanchor='top',
+                                 font=dict(size=15),
+                                 x=0.13,
+                                 y=0.95))
+    # Update the title using the annotation, which is the only way
+    # to display the y-axis horizontally with a title
+    fig.add_annotation(text="Visualisation de la distribution "+Variable_options[colored]+" dans les disciplines du domaine "+domaine,
+                       y=1.09,
                        showarrow=False,
-                       font=dict(size=16),
                        align='left',
                        xref='paper',
-                       yref='paper')
-    fig.update_layout(margin=dict(t=100))
-
-    if mode == 'count':
-        fig.update_yaxes(title_text='')
-        fig.update_layout(title=dict(text='Nombre de publications',
-                                     xanchor='left',
-                                     yanchor='top',
-                                     font=dict(size=15),
-                                     y=0.88))
-    elif mode == 'percentage':
-        fig.update_yaxes(title_text='',
-                         range=[0,100])
-        fig.update_layout(title=dict(text='Pourcentage de publications [%]',
-                                     xanchor='left',
-                                     yanchor='top',
-                                     font=dict(size=15),
-                                     y=0.88))
-        fig.update_traces(hoverlabel=dict(namelength=0))
-
+                       yref='paper',
+                       font=dict(size=18))
+    # Size and title of figure
+    fig.update_layout(height=800, width=1200,
+                      legend_title='Catégories '+Variable_options[colored])
     return fig

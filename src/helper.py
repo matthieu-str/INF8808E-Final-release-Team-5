c0f7e371ff3d8c95ab8bd68fb667693b59bb411b
bessem248 <79014688+bessem248@users.noreply.github.com>
2023-06-29 21:57:36 -0400

    Update helper.py

--- src/helper.py
+++ src/helper.py
@@ -1,25 +1,4 @@
 
-'''
-    This file contains some helper functions to help display some figures.
-'''
-
-
-def update_box_layout(fig, title_text, showlegend=True):
-    '''
-        return Box plots of the number of pages per year for all grades.
-    '''
-    fig.update_layout(
-            title={
-                'text': title_text,
-                'x': 0.5,
-                'xanchor': 'center',
-                'yanchor': 'top'
-                },
-            xaxis_title="Année",
-            yaxis_title="Nombre de Pages",
-            hovermode='x unified',
-            showlegend=showlegend)
-    return fig
 
 
 
